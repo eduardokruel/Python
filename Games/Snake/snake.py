14bb488015fa598aecd4ccd6b51b5c2453d7b75b
eduardokruel <eduardo@kruel.xyz>
2017-12-02 22:03:15 -0200

    Added imports and PyGame error count

---
+++ Games/Snake/snake.py
@@ -0,0 +1,14 @@
+# Snake Game
+
+import pygame
+import sys
+import random
+import time
+
+check_errors = pygame.init()
+
+if check_errors[1] > 0:
+    print("(!) Had {0} initializing errors, exiting...".format(check_errors[1]))
+    sys.exit(-1)
+else:
+    print("(+) PyGame successfully initialized!")
