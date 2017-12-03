c17c623a5071d84b5da10a92436379e70f0d725c
eduardokruel <eduardo@kruel.xyz>
2017-12-02 22:18:40 -0200

    Added Comments and Display Window

--- Games/Snake/snake.py
+++ Games/Snake/snake.py
@@ -1,14 +1,21 @@
 # Snake Game
+# By Eduardo Kruel
 
+# Imports
 import pygame
 import sys
 import random
 import time
 
+# Error check
 check_errors = pygame.init()
-
 if check_errors[1] > 0:
     print("(!) Had {0} initializing errors, exiting...".format(check_errors[1]))
     sys.exit(-1)
 else:
     print("(+) PyGame successfully initialized!")
+
+# Play Surface
+playSurface = pygame.display.set_mode((720,460))
+pygame.display.set_caption('Snake Game by Eduardo Kruel')
+time.sleep(5)
